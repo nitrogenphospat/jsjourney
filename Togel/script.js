58bbe961f97728197502ea6e38887227ec9a0e0d
nitrogenphospat <140783943+nitrogenphospat@users.noreply.github.com>
2024-02-11 15:42:48 +0700

    Add files via upload

---
+++ Togel/script.js
@@ -0,0 +1,6 @@
+
+function duarr(){
+    document.getElementById('angkax').innerText = Math.floor(Math.random()*9)
+    document.getElementById('angkay').innerText = Math.floor(Math.random()*9) 
+    document.getElementById('angkaz').innerText = Math.floor(Math.random()*9)  
+}
